1737760e91553001580d910b17c6a854b8a634f8
Ra <oso_imartinez@hotmail.com>
2024-11-05 20:20:55 -0600

    Empezando el proyecto

--- main.dart
+++ main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_examen_2/router/ListRouters.dart';
+import 'package:flutter_examen_2/router/routers.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      theme: ThemeData().copyWith(
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.blue),
       ),
+      initialRoute: Routers.pantallaLogin,
+      routes: Listrouters.listScreens,
     );
   }
 }
--- screens/login.dart
+++ screens/login.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_examen_2/router/routers.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("hola");
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0), //
+                image: const DecorationImage(
+                  image: AssetImage('assets/logo.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Usuario',
+              ),
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Contraseña',
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  Navigator.pushNamed(context, Routers.pantallaCategorias);
+                },
+                child: const Text("Ingresar"))
+          ],
+        ),
+      ),
+    );
   }
 }
