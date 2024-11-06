bdfbb59ff8abeb3272995e37d55436b1e3d19cf9
Ra <oso_imartinez@hotmail.com>
2024-11-06 12:57:32 -0600

    Borrando archivos duplicados y agregando lib al tracking de git

--- router/routers.dart
+++
@@ -1,9 +0,0 @@
-class Routers {
-  static const pantallaLogin = "login";
-
-  static const pantallaCategorias = "categorias";
-
-  static const pantallaProductos = "productos";
-
-  static const pantallaDetalleProducto = "detalle_producto";
-}
